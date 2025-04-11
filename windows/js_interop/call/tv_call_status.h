4d4942ede10164ba69bfd6f31538eaf39581b5f7
Stanislau Bautovich <stasbav@gmail.com>
2025-04-11 15:28:52 +0900

    remove unused fies

--- windows/js_interop/call/tv_call_status.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-struct TVCallStatus {
-    std::string status;
-    std::string callSid;
-    bool isMuted;
-};
--- windows/js_interop/call/tv_error.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-struct TVError {
-    std::string code;
-    std::string message;
-    std::string description;
-};
